4264b0daabf80b8cb19965ec33a54725cd3d56ad
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2017-12-28 16:44:16 +0100

    Update CALCULADORA 2.py

--- CALCULADORA 2.py
+++ CALCULADORA 2.py
@@ -9,12 +9,13 @@ def btnClik(num):
     input_text.set(operador)
 
 def saludo():
-    print("Hola")#ESTO ES SOLO PROVISIONAL
+    print("HOLA")
+
+    
 
 ancho_boton=11
 alto_boton=3
 
-VALOR=IntVar()
 input_text=StringVar()
 operador=""
 Boton0=Button(ventana,text="0",width=ancho_boton,height=alto_boton,command=lambda:btnClik(0)).place(x=17,y=180)
@@ -27,8 +28,7 @@ Boton6=Button(ventana,text="6",width=ancho_boton,height=alto_boton,command=lambd
 Boton7=Button(ventana,text="7",width=ancho_boton,height=alto_boton,command=lambda:btnClik(7)).place(x=287,y=240)
 Boton8=Button(ventana,text="8",width=ancho_boton,height=alto_boton,command=lambda:btnClik(8)).place(x=17,y=300)
 Boton9=Button(ventana,text="9",width=ancho_boton,height=alto_boton,command=lambda:btnClik(9)).place(x=107,y=300)
-BotonC=Button(ventana,text="C",width=ancho_boton,height=alto_boton,command=saludo).place(x=197,y=300)#!!!!
-
+BotonC=Button(ventana,text="C",width=ancho_boton,height=alto_boton,command=saludo).place(x=197,y=300)#LO DE "saludo" ES PROVISIONAL (SOLO PARA QUE NO DE ERROR)
 #BotonSum=Button(ventana,text="+",command=print(Salida)).place(x=110,y=50)
 
 #Salida=Text(ventana,width=20,height=1)
