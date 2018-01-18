955e9c73e3dab23f7708e068ee54311a78d865e3
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-01-18 20:32:35 +0100

    Update CALCULADORA 2.py

--- CALCULADORA 2.py
+++ CALCULADORA 2.py
@@ -3,7 +3,7 @@ ventana=Tk()
 ventana.title("CALCULADORA")
 ventana.geometry("392x600")
 ventana.configure(background="SkyBlue4")
-color_doton=("light grey")
+color_doton=("gray77")
 from math import *
 
 def btnClik(num):
@@ -57,12 +57,11 @@ BotonSqrt=Button(ventana,text="√",bg=color_doton,width=ancho_boton,height=alto
 BotonParen1=Button(ventana,text="(",bg=color_doton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("(")).place(x=17,y=480)
 BotonParen2=Button(ventana,text=")",bg=color_doton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(")")).place(x=107,y=480)
 BotonResto=Button(ventana,text="%",bg=color_doton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("%")).place(x=197,y=480)
-BotonC=Button(ventana,text="C",bg=color_doton,width=ancho_boton,height=alto_boton,command=clear).place(x=107,y=420)#Quiero que sea "0"
-BotonExp=Button(ventana,text="EXP",bg=color_doton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**")).place(x=197,y=420)#LO DE "saludo" ES PROVISIONAL (SOLO PARA QUE NO DE ERROR)
+BotonC=Button(ventana,text="C",bg=color_doton,width=ancho_boton,height=alto_boton,command=clear).place(x=107,y=420)
+BotonExp=Button(ventana,text="EXP",bg=color_doton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**")).place(x=197,y=420)
 BotonResul=Button(ventana,text="=",bg=color_doton,width=ancho_boton,height=alto_boton,command=operacion).place(x=287,y=420)
 
 Salida=Entry(ventana,font=('arial',20,'bold'),width=22,textvariable=input_text,bd=20,insertwidth=4,bg="powder blue",justify="right").place(x=10,y=60)
 
 
 ventana.mainloop()
-
