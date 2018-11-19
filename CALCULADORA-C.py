3c451a92140f82a604fdfe280022d6edacd6180e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-11-19 10:23:26 +0100

    Update CALCULADORA-C.py

--- CALCULADORA-C.py
+++ CALCULADORA-C.py
@@ -8,48 +8,48 @@ from math import *
 
 def btnClik(num):
     global operador
+    if operador=="ERROR":
+        input_text.set(operador)
+        operador=("")
     operador=operador+str(num)
     input_text.set(operador)
-    
+
+def funci(op):
+    global operador
+    try:
+        operador=str(eval(op+"("+operador+")"))
+    except:
+        clear()
+        operador=("ERROR")
+    input_text.set(operador)
 
 def clear():
     global operador
     operador=("")
     input_text.set("0")
 
-def cambio_signo(): #NO USAR AÚN
+def cambio_signo():
     global operador
     try:
         opera=float(eval(operador))
         operador=str(opera*(-1))
     except:
         input_text.set("ERROR")
-    input_text.set(operador)    
-        
+    input_text.set(operador)
 
 #def cero():
     #global operador
     #clear()
     #input_text.set("0")
 
-def tange(): #NO USAR AÚN
-    global operador
-    print(operador)
-    try:
-        opera=eval("tan("+operador+")")
-    except:
-        clear()
-        opera=("ERROR")
-    input_text.set(opera)
-
 def redondeo():
     global operador
     try:
-        opera=eval("round("+operador+")")
+        operador=str(eval("round("+operador+")"))
     except:
         clear()
-        opera=("ERROR")
-    input_text.set(opera)
+        operador=("ERROR")#Error "ERROR+operador"
+    input_text.set(operador)
 
 
 def operacion():
@@ -70,18 +70,18 @@ input_text=StringVar()
 operador=""
 clear()#MUESTRA VALOR "0" AL INICIAR LA CALCULADORA
 bd=10
-Boton0=Button(ventana,text="0",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(0)).place(x=21,y=180)#+10
-Boton1=Button(ventana,text="1",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(1)).place(x=89-9,y=180)#-9
-Boton2=Button(ventana,text="2",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(2)).place(x=147-8,y=180)#-8
-Boton3=Button(ventana,text="3",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(3)).place(x=205-7,y=180)#-7
-Boton4=Button(ventana,text="4",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(4)).place(x=263-6,y=180)#-6
-Boton5=Button(ventana,text="5",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(5)).place(x=321-5,y=180)#-5
-Boton6=Button(ventana,text="6",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(6)).place(x=21,y=228)#ori240..68
+Boton0=Button(ventana,text="0",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(0)).place(x=21,y=180)
+Boton1=Button(ventana,text="1",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(1)).place(x=89-9,y=180)
+Boton2=Button(ventana,text="2",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(2)).place(x=147-8,y=180)
+Boton3=Button(ventana,text="3",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(3)).place(x=205-7,y=180)
+Boton4=Button(ventana,text="4",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(4)).place(x=263-6,y=180)
+Boton5=Button(ventana,text="5",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(5)).place(x=321-5,y=180)
+Boton6=Button(ventana,text="6",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(6)).place(x=21,y=228)
 Boton7=Button(ventana,text="7",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(7)).place(x=80,y=228)
 Boton8=Button(ventana,text="8",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(8)).place(x=139,y=228)
 Boton9=Button(ventana,text="9",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(9)).place(x=198,y=228)
 BotonC=Button(ventana,text="π",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("pi")).place(x=257,y=228)
-BotonComa=Button(ventana,text=",",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(".")).place(x=316,y=228)###############
+BotonComa=Button(ventana,text=",",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(".")).place(x=316,y=228)
 BotonSuma=Button(ventana,text="+",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("+")).place(x=21,y=276)
 BotonResta=Button(ventana,text="-",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("-")).place(x=80,y=276)
 BotonMulti=Button(ventana,text="*",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("*")).place(x=139,y=276)
@@ -91,13 +91,13 @@ BotonParen1=Button(ventana,text="(",bg=color_boton,width=ancho_boton,height=alto
 BotonParen2=Button(ventana,text=")",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(")")).place(x=80,y=324)
 BotonResto=Button(ventana,text="%",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("%")).place(x=257,y=276)
 Botonln=Button(ventana,text="ln",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("log")).place(x=198,y=324)
-BotonSn=Button(ventana,text="sin",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sin")).place(x=257,y=324)
-BotonCs=Button(ventana,text="cos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("cos")).place(x=316,y=324)
-BotonTn=Button(ventana,text="tan",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("tan")).place(x=21,y=372)
+BotonSn=Button(ventana,text="sin",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:funci("sin")).place(x=257,y=324)
+BotonCs=Button(ventana,text="cos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:funci("cos")).place(x=316,y=324)
+BotonTn=Button(ventana,text="tan",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:funci("tan")).place(x=21,y=372)
 BotonR=Button(ventana,text="R",bg=color_boton,width=ancho_boton,height=alto_boton,command=redondeo).place(x=80,y=372)
 BotonCE=Button(ventana,text="CE",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear).place(x=139,y=372)
-BotonCS=Button(ventana,text="+/-",bg=color_boton,width=ancho_boton,height=alto_boton).place(x=198,y=372)#,command=cambio_signo
-BotonC=Button(ventana,text="C",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear).place(x=316,y=276)
+BotonCS=Button(ventana,text="+/-",bg=color_boton,width=ancho_boton,height=alto_boton,command=cambio_signo).place(x=316,y=276)##############316,276
+BotonC=Button(ventana,text="C",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear).place(x=198,y=372)########################198,372
 BotonExp=Button(ventana,text="EXP",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**")).place(x=139,y=324)
 BotonResul=Button(ventana,text="=",bg=color_boton,width=ancho_boton,height=alto_boton,command=operacion).place(x=287,y=420)
 
