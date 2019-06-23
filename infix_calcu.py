5d43a5186cd8c806c45482a5a96750c24c74d2f1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-23 17:59:02 +0200

    Update infix_calcu.py

--- infix_calcu.py
+++ infix_calcu.py
@@ -2,8 +2,14 @@ from tkinter import *
 ventana=Tk()
 ventana.title("CALCULADORA INFIJA")
 #ventana.configure(background="gray12")
-ventana.geometry("373x490")
+ventana.geometry("360x490")
 
-Entry(ventana,font=('Arial',20,"bold"),width=24,bd=2,bg="PaleGreen3",justify="right").place(x=4,y=30)
+Entry(ventana,font=('Arial',20,"bold"),width=23,bd=2,bg="PaleGreen3",justify="right").place(x=4,y=30)
+
+Button(ventana,text="7",width=7,height=2).place(x=4,y=150)
+Button(ventana,text="8",width=7,height=2).place(x=80,y=150)
+Button(ventana,text="9",width=7,height=2).place(x=152,y=150)
+Button(ventana,text="DEL",width=7,height=2).place(x=224,y=150)
+Button(ventana,text="AC",width=7,height=2).place(x=296,y=150)
 
 ventana.mainloop()
