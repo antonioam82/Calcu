9d0c0793f226069b2d0d0ca2166eba1a04c0f3ef
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-23 17:35:00 +0200

    Create infix_calcu.py

---
+++ infix_calcu.py
@@ -0,0 +1,9 @@
+from tkinter import *
+ventana=Tk()
+ventana.title("CALCULADORA INFIJA")
+#ventana.configure(background="gray12")
+ventana.geometry("373x490")
+
+Entry(ventana,font=('Arial',20,"bold"),width=24,bd=2,bg="PaleGreen3",justify="right").place(x=4,y=30)
+
+ventana.mainloop()
