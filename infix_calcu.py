021c05de99d592243f0764dcd9023ead22ff1e0c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-24 19:09:22 +0200

    Update infix_calcu.py

--- infix_calcu.py
+++ infix_calcu.py
@@ -6,7 +6,7 @@ ventana.geometry("360x490")
 numeroPantalla=StringVar()
 numero=""
 resultado=0
-operacion="+"
+#operacion="+"
 prev_sign=""
 primr=True
 
@@ -20,6 +20,7 @@ def calculo(o):
     global numero
     global primr
     global prev_sign
+    global operacion
     if primr==True:
         resultado+=float(numero)
         prev_sign=o
@@ -29,20 +30,29 @@ def calculo(o):
     else:
         if o==prev_sign:
             if o=="+":
-                resultado+=float(numero)
+                resultado+=float(numero)######
                 print(resultado)
             elif o=="-":
                 resultado-=float(numero)
                 print(resultado)
+            elif o=="*":
+                resultado*=float(numero)
+            elif o=="/":
+                resultado/=float(numero)
         else:
             if prev_sign=="+":
-                resultado+=float(numero)
+                resultado+=float(numero)######
                 print(resultado)
             elif prev_sign=="-":
                 resultado-=float(numero)
                 print(resultado)
+            elif prev_sign=="*":
+                resultado*=float(numero)
+            elif prev_sign=="/":
+                resultado/=float(numero)
             prev_sign=o
     numeroPantalla.set(resultado)
+    operacion=o
     numero=""
 
 def clear():
@@ -55,6 +65,22 @@ def clear():
     primr=True
     prev_sign=""
     numeroPantalla.set(resultado)
+
+def result():
+    global numero
+    global resultado
+    global prev_sign
+    if operacion=="+":
+        resultado+=float(numero)
+    elif operacion=="-":
+        resultado-=float(numero)
+    elif operacion=="*":
+        resultado*=float(numero)
+    elif operacion=="/":
+        resultado/=float(numero)
+    numeroPantalla.set(resultado)
+    prev_sign=operacion
+    numero=""
 
 #clear()
     
@@ -70,7 +96,7 @@ Button(ventana,text="C",width=7,bg="DarkOrange2",height=2).place(x=296,y=180)
 Button(ventana,text="4",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("4")).place(x=4,y=238)
 Button(ventana,text="5",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("5")).place(x=80,y=238)
 Button(ventana,text="6",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("6")).place(x=152,y=238)
-Button(ventana,text="x",width=7,fg="white",bg="gray13",height=2).place(x=224,y=238)
+Button(ventana,text="x",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("*")).place(x=224,y=238)
 Button(ventana,text="%",width=7,fg="white",bg="gray13",height=2).place(x=296,y=238)
 Button(ventana,text="1",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("1")).place(x=4,y=296)
 Button(ventana,text="2",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("2")).place(x=80,y=296)
@@ -78,10 +104,10 @@ Button(ventana,text="3",width=7,fg="white",bg="gray13",height=2,command=lambda:n
 Button(ventana,text="+",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("+")).place(x=224,y=296)
 Button(ventana,text="-",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("-")).place(x=296,y=296)
 Button(ventana,text="0",width=7,fg="white",bg="gray13",height=2).place(x=4,y=354)
-Button(ventana,text="+/-",width=7,fg="white",bg="gray13",height=2).place(x=80,y=354)
+Button(ventana,text="/",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("/")).place(x=80,y=354)
 Button(ventana,text=".",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado(".")).place(x=152,y=354)
 Button(ventana,text="EXP",width=7,fg="white",bg="gray13",height=2).place(x=224,y=354)
-Button(ventana,text="=",width=7,fg="white",bg="gray13",height=2).place(x=296,y=354)
+Button(ventana,text="=",width=7,fg="white",bg="gray13",height=2,command=result).place(x=296,y=354)
 
 ventana.mainloop()
 
