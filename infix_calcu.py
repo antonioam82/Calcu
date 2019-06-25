633572a25a225632c48d4e8e2a62f314837b5768
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-25 07:39:18 +0200

    Update infix_calcu.py

--- infix_calcu.py
+++ infix_calcu.py
@@ -4,11 +4,10 @@ ventana.title("CALCULADORA INFIJA")
 ventana.configure(background="gray36")
 ventana.geometry("360x490")
 numeroPantalla=StringVar()
-numero=""
-resultado=0
-#operacion="+"
-prev_sign=""
-primr=True
+#numero=""
+#resultado=0
+#prev_sign=""
+#primr=True
 
 def numeroPulsado(n):
     global numero
@@ -39,6 +38,8 @@ def calculo(o):
                 resultado*=float(numero)
             elif o=="/":
                 resultado/=float(numero)
+            elif o=="**":
+                resultado=resultado**float(numero)
         else:
             if prev_sign=="+":
                 resultado+=float(numero)######
@@ -50,6 +51,8 @@ def calculo(o):
                 resultado*=float(numero)
             elif prev_sign=="/":
                 resultado/=float(numero)
+            elif prev_sign=="**":
+                resultado=resultado**float(numero)
             prev_sign=o
     numeroPantalla.set(resultado)
     operacion=o
@@ -78,21 +81,21 @@ def result():
         resultado*=float(numero)
     elif operacion=="/":
         resultado/=float(numero)
+    if operacion=="**":
+        resultado=resultado**float(numero)
     numeroPantalla.set(resultado)
     prev_sign=operacion
     numero=""
 
-#clear()
-    
+clear()
 
 Entry(ventana,font=('Arial',30,"bold"),textvariable=numeroPantalla,width=16,bd=2,bg="PaleGreen3",justify="right").place(x=1,y=30)
-
 
 Button(ventana,text="7",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("7")).place(x=4,y=180)
 Button(ventana,text="8",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("8")).place(x=80,y=180)
 Button(ventana,text="9",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("9")).place(x=152,y=180)
 Button(ventana,text="CE",width=7,bg="DarkOrange2",height=2).place(x=224,y=180)
-Button(ventana,text="C",width=7,bg="DarkOrange2",height=2).place(x=296,y=180)
+Button(ventana,text="C",width=7,bg="DarkOrange2",height=2,command=clear).place(x=296,y=180)
 Button(ventana,text="4",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("4")).place(x=4,y=238)
 Button(ventana,text="5",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("5")).place(x=80,y=238)
 Button(ventana,text="6",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("6")).place(x=152,y=238)
@@ -103,10 +106,10 @@ Button(ventana,text="2",width=7,fg="white",bg="gray13",height=2,command=lambda:n
 Button(ventana,text="3",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("3")).place(x=152,y=296)
 Button(ventana,text="+",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("+")).place(x=224,y=296)
 Button(ventana,text="-",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("-")).place(x=296,y=296)
-Button(ventana,text="0",width=7,fg="white",bg="gray13",height=2).place(x=4,y=354)
+Button(ventana,text="0",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado("0")).place(x=4,y=354)
 Button(ventana,text="/",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("/")).place(x=80,y=354)
 Button(ventana,text=".",width=7,fg="white",bg="gray13",height=2,command=lambda:numeroPulsado(".")).place(x=152,y=354)
-Button(ventana,text="EXP",width=7,fg="white",bg="gray13",height=2).place(x=224,y=354)
+Button(ventana,text="EXP",width=7,fg="white",bg="gray13",height=2,command=lambda:calculo("**")).place(x=224,y=354)
 Button(ventana,text="=",width=7,fg="white",bg="gray13",height=2,command=result).place(x=296,y=354)
 
 ventana.mainloop()
