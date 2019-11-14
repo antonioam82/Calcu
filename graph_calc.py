92fc3d88c165bb24b810262be9cac5670c4e8314
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-14 12:32:53 +0100

    Update graph_calc.py

--- graph_calc.py
+++ graph_calc.py
@@ -4,4 +4,14 @@ import string
 
 Calculadora = Tk()
 
+display = Pmw.ScrolledText(Calculadora, hscrollmode='dynamic',
+                      vscrollmode='dynamic', hull_relief='sunken',
+                      hull_background='gray40', hull_borderwidth=10, 
+                      text_background='honeydew4', text_width=16, #ancho pantalla
+                      text_foreground='black', text_height=6, #alto pantalla
+          text_padx=10, text_pady=10, text_relief='groove',
+                      text_font=('arial', 12, 'bold'))
+
+display.pack(padx=1,pady=1)
+
 Calculadora.mainloop()
