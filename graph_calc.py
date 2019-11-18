b7b2999646f5ba92c8316c1c453d3e53971acbaf
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-18 20:10:56 +0100

    Update graph_calc.py

--- graph_calc.py
+++ graph_calc.py
@@ -2,21 +2,29 @@ from tkinter import *
 import Pmw
 import string
 
-#Calculadora = Pmw.initialise(fontScheme = 'pmw1')
-Calculadora = Tk()
+Calculadora = Pmw.initialise(fontScheme = 'pmw1')
+#Calculadora = Tk()
 Calculadora.title("GRAPH_CALC")
 
 #PANTALLA
 display = Pmw.ScrolledText(Calculadora, hscrollmode='none',#dynamic
                       vscrollmode='none', hull_relief='sunken',#vscrollmode=dynamic
                       hull_background='gray40', hull_borderwidth=10, 
-                      text_background='honeydew4', text_width=26, #ancho pantalla
+                      text_background='honeydew4', text_width=28, #ancho pantalla
                       text_foreground='black', text_height=10, #alto pantalla
           text_padx=10, text_pady=10, text_relief='groove',
                       text_font=('arial', 12, 'bold'))
-display.pack(padx=1,pady=1)
-button = Button(Calculadora)
-button.pack(padx=3,pady=3)
+display.pack(padx=0,pady=0)
+buttons = Pmw.ButtonBox(Calculadora)
+buttons.pack(fill='y', expand=1, padx=1, pady=1)
+buttons.alignbuttons()
+
+buttons.add('2nd')
+buttons.add('Mode')
+buttons.add('Del')
+buttons.add('Alpha')
+buttons.add('Stat')
+#buttons.add('Quit')
 
 
 
