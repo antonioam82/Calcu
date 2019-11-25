d09d5a3a5c3949abbd0c0e14666060bd7c68816b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-25 20:35:05 +0100

    Update graph_calc.py

--- graph_calc.py
+++ graph_calc.py
@@ -16,12 +16,12 @@ display = Pmw.ScrolledText(Calculadora, hscrollmode='none',#dynamic
           text_padx=10, text_pady=10, text_relief='groove',
                       text_font=('arial', 12, 'bold'))
 display.pack(padx=0,pady=0)
-buttons1 = Pmw.ButtonBox(Calculadora,
-                         hull_background='gray40')#frame_borderwidth=2
+buttons1 = Pmw.ButtonBox(Calculadora
+                         ,hull_background='gray40')#frame_borderwidth=2
 buttons1.pack(fill='both', expand=1, padx=1, pady=1)
 #buttons1.alignbuttons()
 
-buttons1.add('2nd',width=5,bg='steelblue1',fg='white')
+buttons1.add('2nd',width=5,bg='steelblue3',fg='white')
 buttons1.add('Mode',bg='gray30',fg='white')
 buttons1.add('Del',bg='gray30',fg='white')
 buttons1.add('Alpha',bg='gray50',fg='white')
@@ -50,7 +50,7 @@ buttons4.add('X2',width=5,fg='white',bg='gray30')
 buttons4.add(",",fg='white',bg='gray30')
 buttons4.add("(",fg='white',bg='gray30')
 buttons4.add(")",fg='white',bg='gray30')
-buttons4.add("/",bg='steelblue1',fg='white')
+buttons4.add("/",bg='steelblue3',fg='white')
 
 buttons5 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
 buttons5.pack(fill='y', expand=1, padx=1, pady=1)
@@ -58,7 +58,7 @@ buttons5.add('Log',width=5,bg='gray30',fg='white')
 buttons5.add("7",bg='gray50',fg='white')
 buttons5.add("8",bg='gray50',fg='white')
 buttons5.add("9",bg='gray50',fg='white')
-buttons5.add("x",bg='steelblue1',fg='white')
+buttons5.add("x",bg='steelblue3',fg='white')
 
 buttons6 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
 buttons6.pack(fill='y', expand=1, padx=1, pady=1)
@@ -66,7 +66,7 @@ buttons6.add('Ln',width=5,bg='gray30',fg='white')
 buttons6.add("4",bg='gray50',fg='white')
 buttons6.add("5",bg='gray50',fg='white')
 buttons6.add("6",bg='gray50',fg='white')
-buttons6.add("-",bg='steelblue1',fg='white')
+buttons6.add("-",bg='steelblue3',fg='white')
 
 buttons7 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
 buttons7.pack(fill='y', expand=1, padx=1, pady=1)
@@ -74,7 +74,7 @@ buttons7.add('STO',width=5,bg='gray30',fg='white')
 buttons7.add("1",bg='gray50',fg='white')
 buttons7.add("2",bg='gray50',fg='white')
 buttons7.add("3",bg='gray50',fg='white')
-buttons7.add("+",bg='steelblue1',fg='white')
+buttons7.add("+",bg='steelblue3',fg='white')
 
 buttons8 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
 buttons8.pack(fill='y', expand=1, padx=1, pady=1)
@@ -82,7 +82,7 @@ buttons8.add('Off',width=5,bg='gray30',fg='white')
 buttons8.add("0",bg='gray50',fg='white')
 buttons8.add(".",bg='gray50',fg='white')
 buttons8.add("(-)",bg='gray50',fg='white')
-buttons8.add("Enter",bg='steelblue1',fg='white')
+buttons8.add("Enter",bg='steelblue3',fg='white')
 
 bts = (buttons1,buttons2,buttons3,buttons4,buttons5,buttons6,
        buttons7,buttons8)
