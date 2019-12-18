6d4f57fb9ef6d6a3c77adeafb68c2de0f206b023
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-18 13:06:44 +0100

    Update graph_calc.py

--- graph_calc.py
+++ graph_calc.py
@@ -1,5 +1,6 @@
 #from tkinter import *
 import Pmw
+from tkinter import Button, Label
 import string
 from math import *
 import numpy as np
@@ -8,6 +9,7 @@ Calculadora = Pmw.initialise(fontScheme = 'pmw1')
 #Calculadora = Tk()
 Calculadora.title("GRAPH_CALC")
 Calculadora.config(bg='gray40')
+Calculadora.geometry("321x500")
 formula = ""
 result = ""
 mem = ""
@@ -90,83 +92,22 @@ display = Pmw.ScrolledText(Calculadora, hscrollmode='none',#dynamic
                       text_foreground='black', text_height=9,#alto pantalla
           text_padx=10, text_pady=10, text_relief='groove',
                       text_font=('Arial', 12, 'bold') )
-display.pack(padx=0,pady=0)
-
-buttons1 = Pmw.ButtonBox(Calculadora,
-                         hull_background='gray40')
-
-buttons1.pack(fill='both', expand=1, padx=1, pady=1)
-#buttons1.alignbuttons()
-
-buttons1.add('2nd',width=5,bg='steelblue3',fg='white',command=nd)
-buttons1.add('Mode',bg='gray30',fg='white')
-buttons1.add('Del',bg='gray30',fg='white',command=del_)
-buttons1.add('Alpha',bg='gray50',fg='white')
-buttons1.add('Stat',bg='gray30',fg='white')
-    
-buttons2 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons2.pack(fill='both', expand=1, padx=1, pady=1)
-#buttons2.alignbuttons()
-buttons2.add('Math',width=5,fg='white',bg='gray30',command=lambda:typer("MATH"))#,command=lambda:typer("MATH")
-buttons2.add("Mtrx",fg='white',bg='gray30',command=lambda:typer("MTRX"))#
-buttons2.add("Pgrm",fg='white',bg='gray30')
-buttons2.add("Vars",fg='white',bg='gray30')
-buttons2.add("Clr",fg='white',bg='gray30')
-
-buttons3 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons3.pack(fill='both', expand=1, padx=1, pady=1)
-buttons3.add('X-1',width=5,fg='white',bg='gray30')
-buttons3.add("Sin",fg='white',bg='gray30',command=lambda:push("sin("))
-buttons3.add("Cos",fg='white',bg='gray30',command=lambda:push("cos("))
-buttons3.add("Tan",fg='white',bg='gray30',command=lambda:push("tan("))
-buttons3.add("^ π",fg='white',bg='gray30',command=pi)
-
-buttons4 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons4.pack(fill='both', expand=1, padx=1, pady=1)
-buttons4.add('X2',width=5,fg='white',bg='gray30')
-buttons4.add(",",fg='white',bg='gray30',command=lambda:push(","))
-buttons4.add("(",fg='white',bg='gray30',command=lambda:push("("))
-buttons4.add(")",fg='white',bg='gray30',command=lambda:push(")"))
-buttons4.add("/",bg='steelblue3',fg='white',command=lambda:push("/"))
-
-buttons5 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons5.pack(fill='both', expand=1, padx=1, pady=1)
-buttons5.add('Log',width=5,bg='gray30',fg='white',command=lambda:push("log("))
-buttons5.add("7",bg='gray50',fg='white',command=lambda:push("7"))
-buttons5.add("8",bg='gray50',fg='white',command=lambda:push("8"))
-buttons5.add("9",bg='gray50',fg='white',command=lambda:push("9"))
-buttons5.add("x",bg='steelblue3',fg='white',command=lambda:push("*"))
-
-buttons6 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons6.pack(fill='both', expand=1, padx=1, pady=1)
-buttons6.add('Ln',width=5,bg='gray30',fg='white')
-buttons6.add("4",bg='gray50',fg='white',command=lambda:push("4"))
-buttons6.add("5",bg='gray50',fg='white',command=lambda:push("5"))
-buttons6.add("6",bg='gray50',fg='white',command=lambda:push("6"))
-buttons6.add("-",bg='steelblue3',fg='white',command=lambda:push("-"))
-
-buttons7 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons7.pack(fill='both', expand=1, padx=1, pady=1)
-buttons7.add('STO',width=5,bg='gray30',fg='white',command=store)
-buttons7.add("1",bg='gray50',fg='white',command=lambda:push("1"))
-buttons7.add("2",bg='gray50',fg='white',command=lambda:push("2"))
-buttons7.add("3",bg='gray50',fg='white',command=lambda:push("3"))
-buttons7.add("+",bg='steelblue3',fg='white',command=lambda:push("+"))
-
-buttons8 = Pmw.ButtonBox(Calculadora,hull_background='gray40')
-buttons8.pack(fill='both', expand=1, padx=1, pady=1)
-buttons8.add('Off',width=5,bg='gray30',fg='white',command=clear)
-buttons8.add("0",bg='gray50',fg='white',command=lambda:push("0"))
-buttons8.add(".",bg='gray50',fg='white',command=lambda:push("."))
-buttons8.add("(-)",bg='gray50',fg='white',command=change_sign)
-buttons8.add("Enter",bg='steelblue3',fg='white',command=operation)
-
-bts = (buttons1,buttons2,buttons3,buttons4,buttons5,buttons6,
-       buttons7,buttons8)
-for i in bts:
-    i.alignbuttons()
+display.place(x=0,y=0)
 
 
+
+Label(master=Calculadora,text='Quit',fg='steelblue3',bg='gray40').place(x=69,y=215)
+Label(master=Calculadora,text='Ins',fg='steelblue3',bg='gray40').place(x=133,y=215)
+Label(master=Calculadora,text='Lock',fg='steelblue3',bg='gray40').place(x=197,y=215)
+Label(master=Calculadora,text='List',fg='steelblue3',bg='gray40').place(x=261,y=215)
+#buttons1.pack(fill='both', expand=1, padx=1, pady=1)
+
+
+Button(Calculadora,text='2nd',bg='steelblue3',fg='white',width=5).place(x=5,y=236) #activebackground="blue".place(x=8,y=243)
+Button(Calculadora,text='Mode',bg='gray30',fg='white',width=5).place(x=69,y=236)
+Button(Calculadora,text='Del',bg='gray30',fg='white',width=5).place(x=133,y=236)
+Button(Calculadora,text='Alpha',bg='gray50',fg='white',width=5).place(x=197,y=236)
+Button(Calculadora,text='Stat',bg='gray30',fg='white',width=5).place(x=261,y=236)
 Calculadora.mainloop()
 
 
