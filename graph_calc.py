6f85b05fcf4aa4e02ea22a68a6acfd58e6c560bd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-04-27 16:58:46 +0200

    Update graph_calc.py

--- graph_calc.py
+++ graph_calc.py
@@ -6,6 +6,7 @@ from math import *
 import numpy as np
 
 Calculadora = Pmw.initialise(fontScheme = 'pmw1')
+#Calculadora = Tk()
 Calculadora.title("GRAPH_CALC")
 Calculadora.config(bg='gray40')
 Calculadora.geometry("321x668")
@@ -16,115 +17,6 @@ ndact = False
 type_op = "MATH"
 matrix=[]
 
-def push(car):
-    global formula
-    formula=formula+car
-    display.appendtext(car)
-    if type_op=="MATRX" and formula!="":
-        if car=="]":
-            matrix.append(formula)
-            display.appendtext("\n")
-            #formula=
-
-def typer(m):
-    global type_op
-    type_op = m
-    print(type_op)
-    clear()
-    #display.appendtext(type_op+"\n")
-
-def del_():
-    global mem
-    if mem!="":
-        mem=""
-
-def twond_():
-    global ndact
-    if ndact == True:
-        ndact = False
-        btTwond.config(bg='steelblue3',fg='white')
-    else:
-        ndact = True
-        btTwond.config(bg='white',fg='steelblue3')
-        
-    nd()
-    ##################################################################
-    
-def clear():
-    global formula, mem, result, ndact
-    formula = ""
-    mem = ""
-    result = ""
-    ndact = False
-    display.clear()
-
-def store():
-    global mem
-    if mem=="" and result!="" and result!="ERROR":
-        mem = str(result)
-        #display.appendtext("STORED: "+mem+"\n\n")
-    else:
-        push(mem)
-    
-def matr_demo():
-    a1 = np.array([[1,2,3],[1,2,8]], float)
-    a2 = np.array([[4,5,6],[2,3,4]], float)
-    print(a1)
-    display.appendtext(a1+a2)
-
-def change_sign():
-    global result
-    if result!="ERROR" and result!="":
-        result = eval(str(result)+"*(-1)")
-        text = '{:^29}'.format(str(result))
-        display.appendtext((text)+"\n")
-
-def nd():
-    if ndact == True:
-        btDiv.configure(command=lambda:push("sqrt("))
-        btPhin.configure(command=pi)
-        btMult.configure(command=lambda:push("["))
-        btRest.configure(command=lambda:push("]"))
-        btOpen.configure(command=lambda:push("{"))
-        btClose.configure(command=lambda:push("}"))
-        btChang.configure(command=lambda:push("abs("))
-        btSin.configure(command=lambda:push("asin("))
-        btCos.configure(command=lambda:push("acos("))
-        btTan.configure(command=lambda:push("atan("))
-        btg.configure(command=lambda:push(str(result)))
-        btLoga.configure(command=lambda:push("pow("))
-        
-    else:
-        btDiv.configure(command=lambda:push("/"))
-        #btPhin.configure(command...
-        btMult.configure(command=lambda:push("*"))
-        btRest.configure(command=lambda:push("-"))
-        btOpen.configure(command=lambda:push("("))
-        btClose.configure(command=lambda:push(")"))
-        btChang.configure(command=change_sign)
-        btSin.configure(command=lambda:push("sin("))
-        btCos.configure(command=lambda:push("cos("))
-        btTan.configure(command=lambda:push("tan("))
-        btLoga.configure(command=lambda:push("log("))
-        btComa.config(command=lambda:push(","))
-        btSum.config(command=lambda:push("+"))
-        
-def pi():
-    if ndact == True:
-        push('3.141592653589793')
-        
-def operation():
-    global formula, result
-    print(formula)
-    if type_op=="MATH" and formula!="":
-        try:
-            result = eval(formula)
-        except:
-            result = "ERROR"
-        text = '{:^30}'.format(str(result))
-        display.appendtext("\n"+text+"\n\n")
-        formula=""
-    #elif type_op=="MATRX":
         
 
 
@@ -168,15 +60,15 @@ for l in labels:
            #'x','Ln','4','5','6','-','STO','1','2','3','+','Off','0','.','(-)','Enter']
 
 
-btTwond=Button(Calculadora,text='2nd',bg='steelblue3',fg='white',width=5,command=twond_)
+btTwond=Button(Calculadora,text='2nd',bg='steelblue3',fg='white',width=5)
 btTwond.place(x=5,y=236) #################################activebackground="blue".place(x=8,y=243)
 Button(Calculadora,text='Mode',bg='gray30',fg='white',width=5).place(x=69,y=236)
-Button(Calculadora,text='Del',bg='gray30',fg='white',width=5,command=del_).place(x=133,y=236)
+Button(Calculadora,text='Del',bg='gray30',fg='white',width=5).place(x=133,y=236)
 Button(Calculadora,text='Alpha',bg='gray50',fg='white',width=5).place(x=197,y=236)
 Button(Calculadora,text='Stat',bg='gray30',fg='white',width=5).place(x=261,y=236)
 
 Button(Calculadora,text='Math',bg='gray30',fg='white',width=5).place(x=5,y=291)
-Button(Calculadora,text='Mtrx',bg='gray30',fg='white',width=5,command=lambda:typer("MATRX")).place(x=69,y=291)
+Button(Calculadora,text='Mtrx',bg='gray30',fg='white',width=5).place(x=69,y=291)
 Button(Calculadora,text='Prgm',bg='gray30',fg='white',width=5).place(x=133,y=291)
 Button(Calculadora,text='Vars',bg='gray30',fg='white',width=5).place(x=197,y=291)
 Button(Calculadora,text='Clr',bg='gray30',fg='white',width=5).place(x=261,y=291)
@@ -200,38 +92,36 @@ btOpen.place(x=133,y=401)
 btClose=Button(Calculadora,text=')',bg='gray30',fg='white',width=5)
 btClose.place(x=197,y=401)
 btDiv=Button(Calculadora,text='/',bg='steelblue3',fg='white',width=5)
-btDiv.place(x=261,y=401)################################################################################################################
+btDiv.place(x=261,y=401)
 
 btLoga=Button(Calculadora,text='Log',bg='gray30',fg='white',width=5)
 btLoga.place(x=5,y=456)
-Button(Calculadora,text='7',bg='gray50',fg='white',width=5,command=lambda:push("7")).place(x=69,y=456)
-Button(Calculadora,text='8',bg='gray50',fg='white',width=5,command=lambda:push("8")).place(x=133,y=456)
-Button(Calculadora,text='9',bg='gray50',fg='white',width=5,command=lambda:push("9")).place(x=197,y=456)
+Button(Calculadora,text='7',bg='gray50',fg='white',width=5).place(x=69,y=456)
+Button(Calculadora,text='8',bg='gray50',fg='white',width=5).place(x=133,y=456)
+Button(Calculadora,text='9',bg='gray50',fg='white',width=5).place(x=197,y=456)
 btMult=Button(Calculadora,text='x',bg='steelblue3',fg='white',width=5)
 btMult.place(x=261,y=456)
 
 Button(Calculadora,text='Ln',bg='gray30',fg='white',width=5).place(x=5,y=511)
-Button(Calculadora,text='4',bg='gray50',fg='white',width=5,command=lambda:push("4")).place(x=69,y=511)
-Button(Calculadora,text='5',bg='gray50',fg='white',width=5,command=lambda:push("5")).place(x=133,y=511)
-Button(Calculadora,text='6',bg='gray50',fg='white',width=5,command=lambda:push("6")).place(x=197,y=511)
+Button(Calculadora,text='4',bg='gray50',fg='white',width=5).place(x=69,y=511)
+Button(Calculadora,text='5',bg='gray50',fg='white',width=5).place(x=133,y=511)
+Button(Calculadora,text='6',bg='gray50',fg='white',width=5).place(x=197,y=511)
 btRest=Button(Calculadora,text='-',bg='steelblue3',fg='white',width=5)
 btRest.place(x=261,y=511)
 
-Button(Calculadora,text='STO',bg='gray30',fg='white',width=5,command=store).place(x=5,y=566)
-Button(Calculadora,text='1',bg='gray50',fg='white',width=5,command=lambda:push("1")).place(x=69,y=566)
-Button(Calculadora,text='2',bg='gray50',fg='white',width=5,command=lambda:push("2")).place(x=133,y=566)
-Button(Calculadora,text='3',bg='gray50',fg='white',width=5,command=lambda:push("3")).place(x=197,y=566)
+Button(Calculadora,text='STO',bg='gray30',fg='white',width=5).place(x=5,y=566)
+Button(Calculadora,text='1',bg='gray50',fg='white',width=5).place(x=69,y=566)
+Button(Calculadora,text='2',bg='gray50',fg='white',width=5).place(x=133,y=566)
+Button(Calculadora,text='3',bg='gray50',fg='white',width=5).place(x=197,y=566)
 btSum=Button(Calculadora,text='+',bg='steelblue3',fg='white',width=5)
 btSum.place(x=261,y=566)
 
-Button(Calculadora,text='Off',bg='gray30',fg='white',width=5,command=clear).place(x=5,y=621)
-Button(Calculadora,text='0',bg='gray50',fg='white',width=5,command=lambda:push("0")).place(x=69,y=621)
-Button(Calculadora,text='.',bg='gray50',fg='white',width=5,command=lambda:push(".")).place(x=133,y=621)
+Button(Calculadora,text='Off',bg='gray30',fg='white',width=5).place(x=5,y=621)
+Button(Calculadora,text='0',bg='gray50',fg='white',width=5).place(x=69,y=621)
+Button(Calculadora,text='.',bg='gray50',fg='white',width=5).place(x=133,y=621)
 btg=Button(Calculadora,text='(-)',bg='gray50',fg='white',width=5)
 btg.place(x=197,y=621)
-Button(Calculadora,text='Enter',bg='steelblue3',fg='white',width=5,command=operation).place(x=261,y=621)
-
-nd()
+Button(Calculadora,text='Enter',bg='steelblue3',fg='white',width=5).place(x=261,y=621)
 
 Calculadora.mainloop()
 
