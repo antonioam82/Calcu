a823d7c07ecccb075090435c0dc7c7da2a3f4e03
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-11-29 11:18:19 +0100

    Update CALCULADORA 2.py

--- CALCULADORA 2.py
+++ CALCULADORA 2.py
@@ -2,38 +2,47 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
 from math import *
-ventana=Tk()
-ventana.title("CALCULADORA")
-ventana.geometry("392x600")
-ventana.configure(background="SkyBlue4")#
-color_boton=("gray77")
 
-
+#VISUALIZAR LA OPERACION EN LA PANTALLA
 def btnClik(num):
     global operador
     operador=operador+str(num)
-    input_text.set(operador) #VISUALIZAR LA OPERACION EN LA PANTALLA
-    
+    input_text.set(operador)
+
+#CÁLCULO Y MUESTRA DE RESULTADOS.
+def resultado():
+    global operador
+    try:
+        opera=str(eval(operador))
+    except:
+        clear()
+        opera=("ERROR")
+    input_text.set(opera)
+
+#LIMPIEZA DE PANTALLA.
 def clear():
     global operador
     operador=("")
     input_text.set("0")
+    
 
-def operacion():
-    global operador
-    try:
-        opera=str(eval(operador))#REALIZAR LA OPERACIÓN PREVIAMENTE VISUALIZADA EN PANTALLA
-    except:
-        clear()
-        opera=("ERROR")
-    input_text.set(opera)#MUESTRA EL RESULTADO
-
+ventana=Tk()
+ventana.title("CALCULADORA")
+ventana.geometry("392x600")
+ventana.configure(background="SkyBlue4")
+color_boton=("gray77")
 
 ancho_boton=11
 alto_boton=3
 input_text=StringVar()
 operador=""
-clear()
+
+Salida=Entry(ventana,font=('arial',20,'bold'),width=22,
+textvariable=input_text,bd=20,insertwidth=4,bg="powder blue",justify="right")
+Salida.place(x=10,y=60)
+
+#AÑADIR BOTONES.
+#CREAMOS NUESTROS BOTONES
 Button(ventana,text="0",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(0)).place(x=17,y=180)
 Button(ventana,text="1",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(1)).place(x=107,y=180)
 Button(ventana,text="2",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(2)).place(x=197,y=180)
@@ -57,8 +66,8 @@ Button(ventana,text="%",bg=color_boton,width=ancho_boton,height=alto_boton,comma
 Button(ventana,text="ln",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("log(")).place(x=287,y=480)
 Button(ventana,text="C",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear).place(x=107,y=420)
 Button(ventana,text="EXP",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**")).place(x=197,y=420)
-Button(ventana,text="=",bg=color_boton,width=ancho_boton,height=alto_boton,command=operacion).place(x=287,y=420)
+Button(ventana,text="=",bg=color_boton,width=ancho_boton,height=alto_boton,command=resultado).place(x=287,y=420)
 
-Salida=Entry(ventana,font=('arial',20,'bold'),width=22,textvariable=input_text,bd=20,insertwidth=4,bg="powder blue",justify="right").place(x=10,y=60)
+input_text.set("0")
 
 ventana.mainloop()
