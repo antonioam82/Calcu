1dfc3fefe5f8aa74a40889f5452473745814271c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-11-26 20:48:01 +0100

    Create SCTFC_CALC.py

---
+++ SCTFC_CALC.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from math import *
+
+class calc:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("SCTFC CALCULATOR")
+        self.root.geometry("860x400")
+
+        self.root.mainloop()
+
+if __name__=="__main__":
+    calc()
