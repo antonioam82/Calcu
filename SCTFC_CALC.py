c3d809422b8627fd654154bae8ea52fccf57e23a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-12-06 11:41:17 +0100

    Update SCTFC_CALC.py

--- SCTFC_CALC.py
+++ SCTFC_CALC.py
@@ -7,6 +7,7 @@ class calc:
         self.root.title("SCTFC CALCULATOR")
         self.root.geometry("653x342")
 
+        self.inverted = False
         self.display=StringVar()
         self.display.set("0")
 
@@ -21,7 +22,7 @@ class calc:
         Button(self.root,text=")",width=11,height=2).place(x=372,y=108)
         Button(self.root,text="%",width=11,height=2).place(x=462,y=108)
         Button(self.root,text="AC",width=11,height=2).place(x=552,y=108)
-        Button(self.root,text="Inv",width=11,height=2).place(x=12,y=152)
+        Button(self.root,text="Inv",width=11,height=2,command=self.inv).place(x=12,y=152)
         self.sin_btn=Button(self.root,text="sin",width=11,height=2)
         self.sin_btn.place(x=102,y=152)
         self.ln_btn=Button(self.root,text="ln",width=11,height=2)
@@ -59,6 +60,23 @@ class calc:
         Button(self.root,text="+",width=11,height=2).place(x=552,y=284)
         
         self.root.mainloop()#
+
+    def inv(self):
+        if self.inverted == False:
+            self.sin_btn.configure(text="asin")
+            self.cos_btn.configure(text="acos")
+            self.ln_btn.configure(text="ex")
+            self.log_btn.configure(text="10x")
+            self.tan_btn.configure(text="atan")
+            self.inverted = True
+        else:
+            self.sin_btn.configure(text="sin")
+            self.cos_btn.configure(text="cos")
+            self.ln_btn.configure(text="ln")
+            self.log_btn.configure(text="log")
+            self.tan_btn.configure(text="tan")
+            self.inverted = False
+        
 
 if __name__=="__main__":
     calc()
