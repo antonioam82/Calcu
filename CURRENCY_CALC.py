148c624413888599fd8c22ce9d879a671847aac6
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-11-11 17:09:00 +0100

    Update CURRENCY_CALC.py

--- CURRENCY_CALC.py
+++ CURRENCY_CALC.py
@@ -19,6 +19,8 @@ class Currency_calc:
         sorted_currencies = sorted(self.currencies.keys())
 
         self.current_dir = tk.StringVar()
+        self.amount = tk.StringVar()
+        #self.amount.set(" ")
         self.current_dir.set(os.getcwd())
 
         entry_dir = tk.Entry(self.root,textvariable=self.current_dir,width=90)
@@ -30,27 +32,31 @@ class Currency_calc:
         currency_selector2["values"] = list(self.currencies.keys())
         currency_selector2.place(x=317, y=80)
 
-        amount_entry = tk.Entry(self.root,width=33,font=('Arial',20,"bold"))
-        amount_entry.place(x=20, y=140)
+        self.amount_entry = tk.Entry(self.root,textvariable=self.amount,width=33,font=('Arial',20,"bold"))
+        self.amount_entry.place(x=20, y=140)
         result_label = tk.Label(self.root,width=29,font=('Arial',20,"bold"),bg="blue")
         result_label.place(x=20,y=190)
 
-        tk.Button(self.root,text="7",width=21,height=2).place(x=20,y=245)
-        tk.Button(self.root,text="8",width=21,height=2).place(x=192,y=245)
-        tk.Button(self.root,text="9",width=21,height=2).place(x=362,y=245)
-        tk.Button(self.root,text="4",width=21,height=2).place(x=20,y=300)
-        tk.Button(self.root,text="5",width=21,height=2).place(x=192,y=300)
-        tk.Button(self.root,text="6",width=21,height=2).place(x=362,y=300)
-        tk.Button(self.root,text="1",width=21,height=2).place(x=20,y=355)
-        tk.Button(self.root,text="2",width=21,height=2).place(x=192,y=355)
-        tk.Button(self.root,text="3",width=21,height=2).place(x=362,y=355)
+        tk.Button(self.root,text="7",width=21,height=2,command=lambda:self.btnClick('7')).place(x=20,y=245)
+        tk.Button(self.root,text="8",width=21,height=2,command=lambda:self.btnClick('8')).place(x=192,y=245)
+        tk.Button(self.root,text="9",width=21,height=2,command=lambda:self.btnClick('9')).place(x=362,y=245)
+        tk.Button(self.root,text="4",width=21,height=2,command=lambda:self.btnClick('4')).place(x=20,y=300)
+        tk.Button(self.root,text="5",width=21,height=2,command=lambda:self.btnClick('5')).place(x=192,y=300)
+        tk.Button(self.root,text="6",width=21,height=2,command=lambda:self.btnClick('6')).place(x=362,y=300)
+        tk.Button(self.root,text="1",width=21,height=2,command=lambda:self.btnClick('1')).place(x=20,y=355)
+        tk.Button(self.root,text="2",width=21,height=2,command=lambda:self.btnClick('2')).place(x=192,y=355)
+        tk.Button(self.root,text="3",width=21,height=2,command=lambda:self.btnClick('3')).place(x=362,y=355)
         tk.Button(self.root,text="<<",width=21,height=2).place(x=20,y=410)
-        tk.Button(self.root,text="0",width=21,height=2).place(x=192,y=410)
-        tk.Button(self.root,text=".",width=21,height=2).place(x=362,y=410)
+        tk.Button(self.root,text="0",width=21,height=2,command=lambda:self.btnClick('0')).place(x=192,y=410)
+        tk.Button(self.root,text=".",width=21,height=2,command=lambda:self.btnClick('.')).place(x=362,y=410)
 
         tk.Button(self.root,text="CALCULATE",width=70,height=2).place(x=20,y=465)
 
         self.root.mainloop()
+
+    def btnClick(self,num):
+        self.amount.set(self.amount_entry.get() + num)
+        
 
 if __name__ == '__main__':
     Currency_calc()
